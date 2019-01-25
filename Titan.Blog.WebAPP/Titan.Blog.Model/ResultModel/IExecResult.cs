3858357582810f4e223e942353c823a57700ebd5
HanJunJun <1454055505@qq.com>
2019-01-25 23:15:05 +0800

    api返回格式

--- Titan.Blog.WebAPP/Titan.Blog.Model/ResultModel/IExecResult.cs
+++ Titan.Blog.WebAPP/Titan.Blog.Model/ResultModel/IExecResult.cs
@@ -9,10 +9,15 @@
     {
         /// <summary>
         /// 结果类型
+        /// 0=操作没有引发任何变化，提交取消。
+        /// 1=操作成功。
+        /// 2=操作引发错误。
+        /// 3=指定参数的数据不存在。
+        /// 4=输入信息验证失败。
+        /// 5=登录失效。
+        /// 6=身份认证信息错误。
+        /// 7=未登录。
         /// </summary>
-        /// <value>
-        /// The type of the result.
-        /// </value>
         TResultType ResultType { get; set; }
 
         /// <summary>
